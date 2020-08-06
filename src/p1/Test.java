49a832022146cca5dc4f6411f02e577cca8db3a6
Maulik Kanani <mkanani@rccl.com>
2020-08-06 22:04:13 +0530

    file

--- src/p1/Test.java
+++ src/p1/Test.java
@@ -6,6 +6,10 @@ public class Test {
 		System.out.println("Hello There");
 	}
 	
+	void m3(){
+		System.out.println("Hello There");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Test");
 	}
@@ -13,13 +17,18 @@ public class Test {
 	void main(){
 		System.out.println("Hi Krishna");
 		System.out.println("Hi Maulik");
+		System.out.println("Hi Harsh");
 	}
 	
 	void dispaly(){
-		System.out.println("I am in TechnoCredits");
+		System.out.println("I am in Techno");
 	}
 	
 	void m1(){
+		System.out.println("Nothing to display");
+	}
+
+	void m4(){
 		System.out.println("Nothing to display");
 	}
 }
