9f470ac43fcdb886eb47056c3e8f9783e9e83713
Maulik Kanani <mkanani@rccl.com>
2020-08-06 22:40:37 +0530

    add

--- src/p1/Test.java
+++ src/p1/Test.java
@@ -4,13 +4,31 @@ public class Test {
 
 	void m1(){
 		System.out.println("Hello");
+		System.out.println("HI");
 	}
 	
 	void main(){
+		System.out.println("Hi Maulik");
 		System.out.println("Hi Krishna");
+	}
+	
+	void m4(){
+		System.out.println("Nothing to display123");
+	}
+	
+	void m5(){
+		System.out.println("Nothing to display");
 	}
 	
 	void m2(){
 		System.out.println("Nothing to display");
+	}
+	
+	void m6(){
+		System.out.println("Nothing to display123xdfsd");
+	}
+	
+	void m3(){
+		System.out.println("Nothing to display123");
 	}
 }
