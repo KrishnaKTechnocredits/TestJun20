819bf5dc67411087bf1869b843830a05e9bdd349
Maulik Kanani <mkanani@rccl.com>
2020-08-06 22:07:30 +0530

    added

--- src/p1/Test.java
+++ src/p1/Test.java
@@ -3,7 +3,7 @@ package p1;
 public class Test {
 
 	void m2(){
-		System.out.println("Hello There");
+		System.out.println("Hello");
 	}
 	
 	public static void main(String[] args) {
@@ -13,13 +13,22 @@ public class Test {
 	void main(){
 		System.out.println("Hi Krishna");
 		System.out.println("Hi Maulik");
+		System.out.println("Hi Harsh123");
 	}
 	
 	void dispaly(){
 		System.out.println("I am in TechnoCredits");
 	}
 	
+	void m5(){
+		System.out.println("Nothing to display");
+	}
+	
 	void m1(){
+		System.out.println("Nothing to display");
+	}
+	
+	void m6(){
 		System.out.println("Nothing to display");
 	}
 }
