f3b0a01d7bf79093196c90e4682b5b3c5c26fd0f
Krishna Kanani <kananikrishna427@gmail.com>
2020-08-07 08:48:13 +0530

    add

--- src/p2/Test.java
+++ src/p2/Test.java
@@ -5,7 +5,6 @@ public class Test {
 	void main() {
 		System.out.println("IN branch A");
 		System.out.println("IN branch A1");
-		System.out.println("IN branch B");
 		System.out.println("IN branch BB");
 	}
 }
